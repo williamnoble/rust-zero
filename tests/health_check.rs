164ac13e5fbad979a758c0a02be1def346a069be
William Noble <27914124+williamnoble@users.noreply.github.com>
2022-09-03 09:40:59 +0100

    Fix subscription tracing and extract handler logic

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -93,6 +93,37 @@ error_message
     }
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+
+    // no need to enumerate a tuple
+    for (body, description) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlendoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return a 200 OK when the payload was {}.",
+            description
+        )
+    }
+
+}
+
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
